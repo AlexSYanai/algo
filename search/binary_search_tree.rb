8dfb7548585a9a9e231e62c2b0a43968e4f1912e
AlexSYanai <yanai.alex@gmail.com>
2015-06-11 04:29:16 -0400

    adds depth first search

--- search/binary_search_tree.rb
+++ search/binary_search_tree.rb
@@ -36,26 +36,42 @@ class BinaryTree
 		@ary = []
 	end
 
-	def print_found_node
+	def print_whole_tree
 		@ary.each {|i| puts "#{i} value #{i.value unless i.nil?} #{i.left_child.value unless i.left_child.nil?} #{i.right_child.value unless i.right_child.nil?}"}
 	end
 
-	def breadth_first_search_idk(item)
-		node = breadth_first_search(item)
-		puts "Value: #{node.value unless node.value.nil?} found at: #{node}"
+	def print_found_node(node)
+		node.nil? ? (puts "Target not found") : (puts "Value: #{node.value unless node.value.nil?} found at: #{node}")
 	end
 
-	def breadth_first_search(item)
-    queue = [@root]
+	def breadth_first_search(target)
+		print_found_node(breadth_first(target))
+	end
 
+	def depth_first_search(target)
+		print_found_node(depth_first(target))
+	end
+
+	def breadth_first(target)
+    queue = [@root]
     until queue.empty?
       current_node = queue.shift
-      return current_node if current_node.value == item
+      return current_node if current_node.value == target
       queue.unshift(current_node.left_child) unless current_node.left_child.nil?
       queue.unshift(current_node.right_child) unless current_node.right_child.nil?
     end
-    puts "Item not found"
-    return nil
+    nil
+  end
+
+  def depth_first(target)
+    stack = [@root]
+    until stack.empty?
+      current_node = stack.pop
+      return current_node if current_node.value == target
+      stack << current_node.left_child unless current_node.left_child.nil?
+      stack << current_node.right_child unless current_node.right_child.nil?
+    end
+    nil
   end
 end
 
@@ -63,5 +79,7 @@ tree_array = [28,22,32,38,21,11,26,36]
 root = tree_array[0]
 btree = BinaryTree.new(root)
 btree_array = TreeBuilder.build_tree(btree,tree_array)
-btree.print_found_node
-btree.breadth_first_search_idk(32)
+btree.print_whole_tree
+btree.breadth_first_search(38)
+btree.breadth_first_search(66)
+btree.depth_first_search(32)
