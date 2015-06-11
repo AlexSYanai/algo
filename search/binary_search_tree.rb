985bcc9ff3f8094c9f4966031f2f8cad8a934401
AlexSYanai <yanai.alex@gmail.com>
2015-06-11 02:41:00 -0400

    adds breadth-first search

--- search/binary_search_tree.rb
+++ search/binary_search_tree.rb
@@ -11,18 +11,18 @@ module TreeBuilder
 end
 
 class Node
-	attr_accessor :leftchild, :rightchild, :value
-	def initialize(value,leftchild=nil,rightchild=nil)
+	attr_accessor :left_child, :right_child, :value
+	def initialize(value,left_child=nil,right_child=nil)
 		@value = value
-		@leftchild = leftchild
-		@rightchild = rightchild
+		@left_child = left_child
+		@right_child = right_child
 	end
 	
 	def link(value)
 		if value < self.value
-			self.leftchild.nil? ? self.leftchild = Node.new(value) : self.leftchild.link(value)
+			self.left_child.nil? ? self.left_child = Node.new(value) : self.left_child.link(value)
 		elsif self.value < value
-			self.rightchild.nil? ? self.rightchild = Node.new(value) : self.rightchild.link(value)
+			self.right_child.nil? ? self.right_child = Node.new(value) : self.right_child.link(value)
 		else
 			return self
 		end
@@ -36,13 +36,32 @@ class BinaryTree
 		@ary = []
 	end
 
-	def tb_print
-		@ary.each {|i| puts "#{i} value #{i.value unless i.nil?} #{i.leftchild.value unless i.leftchild.nil?} #{i.rightchild.value unless i.rightchild.nil?}"}
+	def print_found_node
+		@ary.each {|i| puts "#{i} value #{i.value unless i.nil?} #{i.left_child.value unless i.left_child.nil?} #{i.right_child.value unless i.right_child.nil?}"}
 	end
+
+	def breadth_first_search_idk(item)
+		node = breadth_first_search(item)
+		puts "Value: #{node.value unless node.value.nil?} found at: #{node}"
+	end
+
+	def breadth_first_search(item)
+    queue = [@root]
+
+    until queue.empty?
+      current_node = queue.shift
+      return current_node if current_node.value == item
+      queue.unshift(current_node.left_child) unless current_node.left_child.nil?
+      queue.unshift(current_node.right_child) unless current_node.right_child.nil?
+    end
+    puts "Item not found"
+    return nil
+  end
 end
 
-array_sorted = [28,22,32,38,21,11,26,36]
-root = array_sorted[0]
+tree_array = [28,22,32,38,21,11,26,36]
+root = tree_array[0]
 btree = BinaryTree.new(root)
-btree_array = TreeBuilder.build_tree(btree,array_sorted)
-btree.tb_print
+btree_array = TreeBuilder.build_tree(btree,tree_array)
+btree.print_found_node
+btree.breadth_first_search_idk(32)
