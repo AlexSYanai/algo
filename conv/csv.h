ea74f12c20b8b511ab70046d1cf92374c924472c
AlexSYanai <yanai.alex@gmail.com>
2015-10-12 01:21:20 -0400

    added csv parsing in C - WIP

---
+++ conv/csv.h
@@ -0,0 +1,5 @@
+/* Based on 'The Practice of Programming' */
+
+extern char *csvgetline(FILE *f); /* read next input line */
+extern char *csvfield(int n);	    /* return field n */
+extern int csvnfield(void);		    /* return number of fields */
