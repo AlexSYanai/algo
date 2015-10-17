264e396917c4e41c2426f20f0d71aadd52613c6c
AlexSYanai <yanai.alex@gmail.com>
2015-10-17 16:22:26 -0400

    minor convert updates

--- conv/convert.c
+++ conv/convert.c
@@ -5,210 +5,212 @@
 #include "csv.h"
 /* Basic CSV Parsing - WIP */
 
-enum { NOMEM = -2 };          /* out of memory signal */
+enum { NOMEM = -2 };            /* out of memory signal */
 
-static char *line    = NULL;  /* input chars */
-static char *sline   = NULL;  /* line copy used by split */
-static int  maxline  = 0;     /* size of line[] and sline[] */
-static char **field  = NULL;  /* field pointers */
-static int  maxfield = 0;     /* size of field[] */
-static int  nfield   = 0;     /* number of fields in field[] */
-static int  h_count  = 0;     /* counter to check if header */
-static char **h_ary  = NULL;	/* the header */
-static char fieldsep[] = ","; /* field separator chars */
+static int  max_line    = 0;     /* size of line[] and sep_line[] */
+static int  max_field   = 0;     /* size of field[] */
+static int  nfields     = 0;     /* number of fields in field[] */
+static int  h_count     = 0;     /* counter to check if header */
+static char *line       = NULL;  /* input chars */
+static char **field     = NULL;  /* field pointers */
+static char *sep_line   = NULL;  /* line copy used by split */
+static char **headers   = NULL;  /* the header */
+static char field_sep[] = ",";   /* field separator chars */
+
 static char *advquoted(char *);
 static int split(void);
-
 static int endofline(FILE *fin, int c) /* endofline: check for and consume \r, \n, \r\n, or EOF */
 {
-	int eol;
+  int eol;
 
-	eol = (c=='\r' || c=='\n');
-	if (c == '\r') {
-		c = getc(fin);
-		if (c != '\n' && c != EOF)
-			ungetc(c, fin);	/* read too far; put c back */
-	}
-	return eol;
+  eol = (c == '\r' || c == '\n');
+  if (c == '\r') {
+    c = getc(fin);
+    if (c != '\n' && c != EOF) ungetc(c, fin);  /* read too far; put c back */
+  }
+
+  return eol;
 }
 
 static void reset(void) /* reset: set variables back to starting values */
 {
-	free(line);	/* free(NULL) permitted by ANSI C */
-	free(sline);
-	free(field);
-	line = NULL;
-	sline = NULL;
-	field = NULL;
-	maxline = maxfield = nfield = 0;
+  free(line);            /* free(NULL) permitted by ANSI C */
+  free(sep_line);
+  free(field);
+  line     = NULL;
+  sep_line = NULL;
+  field    = NULL;
+  max_line = max_field = nfields = 0;
 }
 
 /* csvgetline:  get one line, grow as needed */
 char *csvgetline(FILE *fin)
-{	
-	int i, c, a;
-	char *newl, *news;
+{
+  int i, c;
+  char *new_line, *new_sep;
 
-	if (line == NULL) {			/* allocate on first call */
-		maxline = maxfield = 1;
-		line = (char *) malloc(maxline);
-		sline = (char *) malloc(maxline);
-		field = (char **) malloc(maxfield*sizeof(field[0]));
-		if (line == NULL || sline == NULL || field == NULL) {
-			reset();
-			return NULL;		/* out of memory */
-		}
-	}
+  if (line == NULL) {      /* allocate on first call */
+    max_line = max_field = 1;
+    line     = (char *)  malloc(max_line);
+    sep_line = (char *)  malloc(max_line);
+    field    = (char **) malloc(max_field * sizeof(field[0]));
 
-	for (i = 0; (c = getc(fin)) != EOF && !endofline(fin,c); i++) {
-		if (i >= maxline - 1) {	/* grow line */
-			maxline *= 2;		      /* double current size */
-			newl = (char *) realloc(line, maxline);
-			if (newl == NULL) {
-				reset();
-				return NULL;
-			}
+    if (line == NULL || sep_line == NULL || field == NULL) {
+      reset();
+      return NULL; /* out of memory */
+    }
+  }
 
-			line = newl;
-			news = (char *) realloc(sline, maxline);
+  for (i = 0; (c = getc(fin)) != EOF && !endofline(fin,c); i++) {
+    if (i >= max_line - 1) {  /* grow line */
+      max_line *= 2;          /* double current size */
+      new_line = (char *) realloc(line, max_line);
 
-			if (news == NULL) {
-				reset();
-				return NULL;
-			}
+      if (new_line == NULL) {
+        reset();
+        return NULL;
+      }
 
-			sline = news;
-		}
+      line = new_line;
+      new_sep = (char *) realloc(sep_line, max_line);
 
-		line[i] = c;
-	}
+      if (new_sep == NULL) {
+        reset();
+        return NULL;
+      }
 
-	line[i] = '\0';
-	if (split() == NOMEM) {
-		reset();
-		return NULL;			/* out of memory */
-	}
+      sep_line = new_sep;
+    }
 
-	// If first line of csv, 
-	if (h_count == 0) {
-		h_ary = (char **) malloc(csvnfield());
-	}
+    line[i] = c;
+  }
 
-	return (c == EOF && i == 0) ? NULL : line;
+  line[i] = '\0';
+  if (split() == NOMEM) {
+    reset();
+    return NULL;      /* out of memory */
+  }
+
+  /* If first line of csv, */
+  if (h_count == 0) {
+    headers = (char **) malloc(csvnfield());
+  }
+
+  return (c == EOF && i == 0) ? NULL : line;
 }
 
 /* split: split line into fields */
 static int split(void)
 {
-	char *p, **newf;
-	char *sepp; /* pointer to temporary separator character */
-	int sepc;   /* temporary separator character */
+  char *p, **newf;
+  char *sepp; /* pointer to temporary separator character */
+  int sepc;   /* temporary separator character */
 
-	nfield = 0;
-	if (line[0] == '\0') return 0;
-	strcpy(sline, line);
-	p = sline;
+  nfields = 0;
+  if (line[0] == '\0') return 0;
+  strcpy(sep_line, line);
+  p = sep_line;
 
-	do {
-		if (nfield >= maxfield) {
-			maxfield *= 2;			/* double current size */
-			newf = (char **) realloc(field, 
-						maxfield * sizeof(field[0]));
+  do {
+    if (nfields >= max_field) {
+      max_field *= 2;      /* double current size */
+      newf = (char **) realloc(field,
+            max_field * sizeof(field[0]));
 
-			if (newf == NULL) return NOMEM;
-			field = newf;
-		}
+      if (newf == NULL) return NOMEM;
+      field = newf;
+    }
 
-		if (*p == '"') {
-			sepp = advquoted(++p);	/* skip initial quote */
-		} else {
-			sepp = p + strcspn(p, fieldsep);
-		}
+    if (*p == '"') {
+      sepp = advquoted(++p);  /* skip initial quote */
+    } else {
+      sepp = p + strcspn(p, field_sep);
+    }
 
-		sepc = sepp[0];
-		sepp[0] = '\0';				/* terminate field */
-		field[nfield++] = p;
-		p = sepp + 1;
-	} while (sepc == ',');
+    sepc = sepp[0];
+    sepp[0] = '\0';        /* terminate field */
+    field[nfields++] = p;
+    p = sepp + 1;
+  } while (sepc == ',');
 
-	return nfield;
+  return nfields;
 }
 
 /* advquoted: quoted field; return pointer to next separator */
 static char *advquoted(char *p)
 {
-	int i, j, k;
+  int i, j, k;
 
-	for (i = j = 0; p[j] != '\0'; i++, j++) {
-		if (p[j] == '"' && p[++j] != '"') {
-			/* copy up to next separator or \0 */
-			k = strcspn(p+j, fieldsep);
-			memmove(p+i, p+j, k);
-			i += k;
-			j += k;
-			break;
-		}
+  for (i = j = 0; p[j] != '\0'; i++, j++) {
+    if (p[j] == '"' && p[++j] != '"') {
+      /* copy up to next separator or \0 */
+      k = strcspn(p+j, field_sep);
+      memmove(p+i, p+j, k);
+      i += k;
+      j += k;
+      break;
+    }
 
-		p[i] = p[j];
-	}
+    p[i] = p[j];
+  }
 
-	p[i] = '\0';
-	return p + j;
-}
-
-// Printing in JSON format to console
-void Json_print(int x)
-{
-	int y;
-
-	if (x != 1) printf("\n  },  \n");
-	printf("  Row %d: {", x);
-
-	for (y = 0; y < nfield-2; y++) {
-		printf("\n    %s: %s,", h_ary[y], csvfield(y));
-	}
-
-	printf("\n    %s: %s", h_ary[nfield-1], csvfield(nfield-1));
+  p[i] = '\0';
+  return p + j;
 }
 
 /* csvfield:  return pointer to n-th field */
 char *csvfield(int n)
 {
-	if (n < 0 || n >= nfield) return NULL;
-	
-	// Assigning h_ary, the header array, if this is the first iteration
-	if (h_count == 0) h_ary[n] = strdup(field[n]);
+  if (n < 0 || n >= nfields) return NULL;
 
-	return field[n];
+  /* Assigning headers, the header array, if this is the first iteration */
+  if (h_count == 0) headers[n] = strdup(field[n]);
+
+  return field[n];
 }
 
-/* csvnfield:  return number of fields */ 
+/* csvnfield:  return number of fields */
 int csvnfield(void)
 {
-	return nfield;
+  return nfields;
+}
+
+/* Printing in JSON format to console */
+void json_print(int x)
+{
+  int y;
+
+  if (x != 1) printf("\n  },  \n");
+  printf("  Row %d: {", x);
+
+  for (y = 0; y < nfields-2; y++) {
+    printf("\n    %s: %s,", headers[y], csvfield(y));
+  }
+
+  printf("\n    %s: %s", headers[nfields-1], csvfield(nfields-1));
 }
 
 /* csvtest main: test CSV library */
 int main(void)
 {
-	int i;
-	int x = 0;
-	char *line;
+  int i;
+  int x = 0;
+  char *line;
 
-	while ((line = csvgetline(stdin)) != NULL) {
-		if (x == 0) {
-			for (i = 0; i < csvnfield(); i++)
-				csvfield(i);
+  while ((line = csvgetline(stdin)) != NULL) {
+    if (x == 0) {
+      for (i = 0; i < csvnfield(); i++)
+        csvfield(i);
 
-			printf("{\n");
-		} else {
-			Json_print(x);
-		}
+      printf("{\n");
+    } else {
+      json_print(x);
+    }
 
-		if (x == 0) h_count = 1;
-		x++;
-	}
-	
-	printf("\n  }  \n}\n");
-	return 0;
+    if (x == 0) h_count = 1;
+    x++;
+  }
+
+  printf("\n  }  \n}\n");
+  return 0;
 }
